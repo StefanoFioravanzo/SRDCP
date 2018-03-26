7dab126f741b463353ce5f54559ebb5801344482
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2018-03-26 10:40:02 +0200

    Improved version of run_sim.sh: 	* Parse CLI arguments 	* Automatically parse results and print average

--- run_sim.sh
+++ run_sim.sh
@@ -1,12 +1,126 @@
 #!/bin/bash
+# Copyright 2018 Stefano Fioravanzo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-ROOT=$1
-echo "Root folder "$1
-for i in {1..4}
+SIMULATION=$1
+NUM_SIMS=$2
+
+# aggregate_stat:
+#     Average results from multiple simulations
+#
+# Args:
+#     $1: DataCollection / SourceRouting averaging
+#     $2: Number of runs of simulation
+aggregate_stat () {
+    echo -n ${2} >> ${SIMULATION}/sim_average.log
+
+    for (( sim=1; sim<=$NUM_SIMS; sim++ ))
+    do
+        grep "Overall PDR" ${SIMULATION}/${sim}/stat_res.log | \
+        sed -n ${1}p | \
+        egrep -o "[0-9]+\.[0-9]+"
+    done | \
+    paste -sd+ - | \
+    awk -v N=$NUM_SIMS '{print "("$1")/"N}' | \
+    bc | \
+    awk '{print " "$1"%"}' >> ${SIMULATION}/sim_average.log
+
+}
+
+# run_simulation:
+#     Run N simulation with different random seeds
+#
+# Args:
+#     $1: Simulation dir path. Location of result log files
+#     $2: Number of runs of simulation
+run_simulation () {
+    echo "Root folder "$1
+    for (( i=1; i<=$NUM_SIMS; i++ ))
+    do
+    	echo "Experiment "$i
+    	mkdir -p $SIMULATION/$i
+    	cooja_nogui test_nogui_dc.csc > $SIMULATION/$i/cooja_output.log
+    	mv ./*.log $SIMULATION/$i
+    	python parse-stats.py $SIMULATION/$i/test.log > $SIMULATION/$i/stat_res.log
+    done
+}
+
+print_help () {
+    echo "Wireless Sensor Netowrk Project"
+    echo "-- Stefano Fioravanzo"
+    echo
+    echo "Commands"
+    echo -e "\t-s|--simulation\tRun the simulation. Default 1 simulation"
+    echo -e "\t-n|--number\tNumber of simulations to run"
+    echo -e "\t-h|--help\tPrint this help and exit"
+
+    exit 0
+}
+
+# Defaults
+RUN=0
+NUM_SIMS=1
+SIMULATION="default"
+
+# Parse CLI arguments
+POSITIONAL=()
+while [[ $# -gt 0 ]]
 do
-	echo "Experiment "$i
-	mkdir -p $ROOT$i
-	cooja_nogui test_nogui_dc.csc > $ROOT$i/cooja_output.log
-	mv ./*.log $ROOT$i
-	python parse-stats.py $ROOT$i/test.log > $ROOT$i/stat_res.log
+key="$1"
+
+case $key in
+    -h|--help)
+    print_help
+    shift
+    ;; # past argument
+    -s|--simulation)
+    SIMULATION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -n|--number)
+    NUM_SIMS="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -r|--run-simulation)
+    RUN=1
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
 done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# echo SIMULATION     = "${SIMULATION}"
+# echo NUMBER OF RUNS = "${NUM_SIMS}"
+# echo RUN            = "${RUN}"
+
+if [[ $RUN -eq "1" ]]; then
+    echo "RUN the simulation"
+    rm -rf $SIMULATION
+    run_simulation $SIMULATION $NUM_SIMS
+fi
+
+# Average simulations results
+rm ${SIMULATION}/sim_average.log
+echo "Simulation ${SIMULATION} summary:" >> ${SIMULATION}/sim_average.log
+aggregate_stat 1 "Data Collection PDR: "
+aggregate_stat 2 "Source Routing PDR: "
+
+# Show summary
+cat ${SIMULATION}/sim_average.log
--- src/my_collect.h
+++ src/my_collect.h
@@ -9,13 +9,13 @@
 #include "core/net/linkaddr.h"
 
 // Allow or not to send topology reports.
-#define TOPOLOGY_REPORT 1
-#define PIGGYBACKING 1
+#define TOPOLOGY_REPORT 0
+#define PIGGYBACKING 0
 
 #define MAX_NODES 30
 #define MAX_PATH_LENGTH 10
 
-#define BEACON_INTERVAL (CLOCK_SECOND*60)
+#define BEACON_INTERVAL (CLOCK_SECOND*30)
 #define BEACON_FORWARD_DELAY (random_rand() % (CLOCK_SECOND*4))
 // Used for topology reports
 // #define TOPOLOGY_REPORT_INIT_DELAY (random_rand() % (CLOCK_SECOND*1))
